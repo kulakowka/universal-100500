419ad3a112fb7f0e00c18cc2e66885108cea3eb0
Anton Kulakov <kulakowka@gmail.com>
2017-03-23 21:11:45 +0300

    Update webpack.server.prod.config.js

--- webpack.server.prod.config.js
+++ webpack.server.prod.config.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const path = require('path')
+const { resolve } = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const commonConfig = require('./webpack.common.config')
 
@@ -11,7 +11,7 @@ module.exports = {
   entry: './server.js',
 
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: resolve(__dirname, 'dist'),
     filename: 'server.js'
   },
 
@@ -26,25 +26,24 @@ module.exports = {
   },
 
   resolve: {
-    modules: ['node_modules', path.resolve( __dirname, 'src' )],
+    modules: ['node_modules', resolve(__dirname, 'src')],
     extensions: ['.jsx', '.js', '.sass'],
     alias: {
-      routes: path.resolve(__dirname, 'src/routes'),
-      components: path.resolve(__dirname, 'src/components'),
-      containers: path.resolve(__dirname, 'src/containers'),
-      stylesheets: path.resolve(__dirname, 'src/stylesheets'),
-      fonts: path.resolve(__dirname, 'src/static/fonts'),
-      uikit: path.resolve(__dirname, 'src/ui-kit'),
-      reducers: path.resolve(__dirname, 'src/reducers'),
-      utils: path.resolve(__dirname, 'src/utils'),
-      images: path.resolve(__dirname, 'src/static/images')
+      routes: resolve(__dirname, 'src/routes'),
+      components: resolve(__dirname, 'src/components'),
+      containers: resolve(__dirname, 'src/containers'),
+      stylesheets: resolve(__dirname, 'src/stylesheets'),
+      fonts: resolve(__dirname, 'src/static/fonts'),
+      uikit: resolve(__dirname, 'src/ui-kit'),
+      reducers: resolve(__dirname, 'src/reducers'),
+      utils: resolve(__dirname, 'src/utils'),
+      images: resolve(__dirname, 'src/static/images')
     }
   },
 
   externals: fs.readdirSync('node_modules').reduce(function (accumulator, module) {
-    accumulator[module] = `commonjs ${module}`;
-
-    return accumulator;
+    accumulator[module] = `commonjs ${module}`
+    return accumulator
   }, {}),
 
   resolveLoader: {
@@ -53,6 +52,6 @@ module.exports = {
   },
 
   plugins: [
-    new ExtractTextPlugin('trash.css'),
+    new ExtractTextPlugin('trash.css')
   ]
 }
